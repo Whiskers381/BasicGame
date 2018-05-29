4ca9b7bbab3ddb437b1255da96a2a28e9e366403
Unknown <Cantwell.josh@gmail.com>
2018-05-29 18:31:17 +0100

    A bit more work on the peripherals class
    Peripherals class now can detect W,A,S,D,LShft,LCtrl,Escp and mouse movement. Running in debug mode will print out a debug message with mouse pos and currently pressed keys. NOTE Escape no longer exits the game, that functionality is to be re added later.

--- 531989sEngine/Peripherals.cs
+++ 531989sEngine/Peripherals.cs
@@ -11,13 +11,15 @@ namespace BasicEngine
     {
 
         //Ignore these for now
-        private bool WkeyDown = false;
-        private bool AkeyDown = false;
-        private bool SkeyDown = false;
-        private bool DkeyDown = false;
-        private bool CtrlkeyDown = false;
-        private bool ShftkeyDown = false;
-        private bool SpacekeyDown = false;
+        //Motion
+        private bool UpKey = false;
+        private bool LeftKey = false;
+        private bool DownKey = false;
+        private bool RightKey = false;
+        private bool CrouchKey = false;
+        private bool SprintKey = false;
+        private bool JumpKey = false;
+        //System
         private bool Escape = false;
 
 
@@ -37,21 +39,46 @@ namespace BasicEngine
             //Updates the mouse and keyboard
             MouseUpdate(game);
             KeyboardUpdate(game);
+            #region Debug writeouts
+#if DEBUG
+            Console.Write("Mouse Pos {0},{1} Current Keys down are", _MousePos[0], _MousePos[1]);
+            if(UpKey)
+            { Console.Write(" W"); }
+            if (LeftKey)
+            { Console.Write(" A"); }
+            if (DownKey)
+            { Console.Write(" S"); }
+            if (RightKey)
+            { Console.Write(" D"); }
+            if (SprintKey)
+            { Console.Write(" Shift"); }
+            if (CrouchKey)
+            { Console.Write(" Ctrl"); }
+            Console.WriteLine();
+#endif
+            #endregion
         }
         public void MouseUpdate(Game1 game)
         {
             MouseState state = Mouse.GetState();
+            #region Mouse Decection
             _MousePos[0] = state.X;
             _MousePos[1] = state.Y;
-#if DEBUG
-            Console.WriteLine( "Mouse X pos, {0} Mouse Y Pos, {1}", _MousePos[0],_MousePos[1] );
-#endif
+            #endregion
         }
         public void KeyboardUpdate(Game1 game)
         {
             KeyboardState _state = Keyboard.GetState();
-            if (_state.IsKeyDown(Keys.Escape)) { game.Exit(); Console.WriteLine("Escape"); }
-            else { Console.WriteLine("No Escape"); }
+            #region Key Detection
+            if (_state.IsKeyDown(Keys.Escape)) { Escape = true; } else { Escape = false; }
+            if (_state.IsKeyDown(Keys.W)) { UpKey = true;}else { UpKey = false; }
+            if (_state.IsKeyDown(Keys.A)) { LeftKey = true; } else { LeftKey = false; }
+            if (_state.IsKeyDown(Keys.S)) { DownKey = true; } else { DownKey = false; }
+            if (_state.IsKeyDown(Keys.D)) { RightKey = true; } else { RightKey = false; }
+            if (_state.IsKeyDown(Keys.LeftShift)) { SprintKey = true; } else { SprintKey = false; }
+            if (_state.IsKeyDown(Keys.LeftControl)) { CrouchKey = true; } else { CrouchKey = false; }
+            #endregion
+
         }
     }
 }
